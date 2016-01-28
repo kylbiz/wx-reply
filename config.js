a55c073a5bdd5e8a86a4aadb1b2a6bdfeaefee35
zunkun <zunkun@zunkun.local>
2016-01-28 16:42:09 +0800

    添加错误回复

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
 	"wx": {
 		appid: "wx51bf6c24340021b0", // wechat appid
-		appsecret: "d3ca2d71ddef3c3e1df7e14c12c52db5" // wechat appsecret
+		appsecret: "d3ca2d71ddef3c3e1df7e14c12c52db5", // wechat appsecret
+		errorMessage: "系统繁忙，请稍后重试！"
 	}
 }
