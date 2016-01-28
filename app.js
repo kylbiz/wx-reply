01c0ecd54ce1e301d7a9f0284326ba15a37ce00f
zunkun <zunkun@zunkun.local>
2016-01-28 15:47:36 +0800

    add config

--- app.js
+++ app.js
@@ -4,17 +4,6 @@ var port = 4987;
 var MessageServer = require('./lib/message');
 var Message = new MessageServer();
 
-var imagePath = './canvas.jpg';
-var gm = require('gm'); 
-
-
-// var wechat = require('wechat');
-// var config = {
-//   token: 'wechat',
-//   appid: 'wx51bf6c24340021b0',
-//   encodingAESKey: '0Isf7ge4uVVDxq7Ich0iRm4vGlZKkwNQR0JOMr3IFXF'
-// };
-
 //-------------------------------------------------
 var server = restify.createServer();
 
@@ -31,14 +20,6 @@ server.use(restify.bodyParser());
 
 //-------------------------------------------------
 server.post('/wechat', Message.getMessage);
-
-// server.post('/post', wechat(config, function (req, res, next) {
-//   // 微信输入信息都在req.weixin上
-//   var message = req.weixin;
-//   console.log(message)
-// }));
-
-
 
 
 //-------------------------------------------------
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	"wx": {
+		appid: "wx51bf6c24340021b0", // wechat appid
+		appsecret: "d3ca2d71ddef3c3e1df7e14c12c52db5" // wechat appsecret
+	}
+}
