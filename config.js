ba2baa014731c79cdb64aaa29aa23315d3729380
zunkun <liuzk552@gmail.com>
2016-01-30 19:22:15 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	"wx": {
-		appid: "wx51bf6c24340021b0", // wechat appid
-		appsecret: "d3ca2d71ddef3c3e1df7e14c12c52db5", // wechat appsecret
+		appid: "your_wx_appid", // wechat appid
+		appsecret: "your_wx_appsecret", // wechat appsecret
 		errorMessage: "系统繁忙，请稍后重试！"
 	}
 }
