5711109b90b6877a05df60a940555ebc46cffa0b
zunkun <zunkun@zunkun.local>
2016-02-17 19:13:27 +0800

    删除错误图片

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
 	"wx": {
 		appid: "your_wx_appid", // wechat appid
 		appsecret: "your_wx_appsecret", // wechat appsecret
-		errorMessage: "系统繁忙，请稍后重试！"
+		errorMessage: "我们正在处理，请稍等！"
 	}
 }
